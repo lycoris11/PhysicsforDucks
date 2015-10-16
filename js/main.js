b00279e3ff6126ce64a9e5a4211d9491233b1e45
EforEpsilon <jerryl3698@gmail.com>
2015-10-16 10:26:47 -0500

    Deletes
    Deleted faulty function

--- js/main.js
+++ js/main.js
@@ -4,10 +4,4 @@ jQuery(document).ready(function($){
 		window.location = "engine.html";
 	});
 
-	$('#create_button').click(function () {
-	    World.add(engine.world, Bodies.rectangle(100,100,100,100);
-	});
-
-
-
 });

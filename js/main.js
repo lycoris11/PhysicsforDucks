b5bfffce52fb6ff666b759938777df1a7055af04
Varun Pinto <varun.pinto98@gmail.com>
2015-10-20 11:15:49 -0500

    Code removal
    I removed some arbitrary code

--- js/main.js
+++ js/main.js
@@ -3,11 +3,4 @@ jQuery(document).ready(function($){
 	$('#start_button').click(function(){
 		window.location = "engine.html";
 	});
-
-	$('#create_button').click(function () {
-	    World.add(engine.world, Bodies.rectangle(100,100,100,100);
-	});
-
-
-
 });
